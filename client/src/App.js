fc56447e121f9d56fe40cffeb9c62a2dccb1bbc9
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-03-29 00:08:20 +0530

    client-setup and Reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Heyyyy</h1>
+     <AppNavbar/>
     </div>
   );
 }
