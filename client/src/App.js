85efc4c77f5d0579cc1cd342f53ecf02aed1dc80
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-03-29 16:15:34 +0530

    Added Shopping List and Transitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <AppNavbar/>
-    </div>
-  );
+class App extends Component{
+  render(){
+   return (
+      <div className="App">
+      <AppNavbar/>
+      <ShoppingList/>
+      </div>
+    );
+  }
 }
 
 export default App;
